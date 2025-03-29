9fe762b17b2a5b4174467f7ca9a6c0e40a6fb159
RohitPrakashMhatre <150694551+RohitPrakashMhatre@users.noreply.github.com>
2025-03-29 11:53:15 +0530

    Update query_1.sql

--- SQL-Queries/query_1.sql
+++ SQL-Queries/query_1.sql
@@ -1,4 +1,6 @@
- SELECT COUNT(user_id) as user_counts, age_group
+# Distribution of subscribers by age group and city tier
+
+SELECT COUNT(user_id) as user_counts, age_group
  FROM subscribers
  GROUP BY age_group
  ORDER BY user_counts desc;
